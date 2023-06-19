22fa7d0c085e35716055ec794a4c4beecc79a093
zoltanmeszaros977 <zoltanmeszaros977@gmail.com>
2023-06-19 21:55:31 +0200

    deploy

--- env.js
+++ env.js
@@ -1,4 +1,4 @@
 window.env = {
-  "API_KEY": "JZLWuJDR9crbBSjYEFfoziVpdkNQq6FNywPhfzdT",
+  "API_KEY": "2dFKeuYrko2WhEJSgZas3jNVvzgU1PelqScnm2tp",
   "API_BASE_URL": "https://api.nasa.gov:443/planetary/apod"
 };
