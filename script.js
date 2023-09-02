ac668e16eea0219b31c1c0157c69f815fbce2d35
zoltanmeszaros977 <zoltanmeszaros977@gmail.com>
2023-09-02 21:15:00 +0200

    publish

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
 async function _load() {
-    const aboutMeArticle = document.querySelector('main > section:first-of-type > article:first-of-type');
-    const techStackTitle = document.querySelector('main > section:first-of-type > article:nth-of-type(2) > h1');
-    const englishTitle = document.querySelector('main > section:first-of-type > article:nth-of-type(3) > h1');
-    const english = document.querySelector('main > section:first-of-type > article:nth-of-type(3) > p');
-    const contactTitle = document.querySelector('main > aside > section > h3');
-    const langSwitch = document.querySelector('[type=checkbox]');
-    
-    langSwitchHandler();
-    
-    async function getTextData(path) {
-        const response = await fetch(path);
-        const json = await response.json();
-        return json;
-    }
-
     async function langSwitchHandler() {
+        async function getTextData(path) {
+            const response = await fetch(path);
+            const json = await response.json();
+            return json;
+        }
+        function renderTextData(textData) {
+            contactTitle.innerText = textData.contactTitle;
+            techStackTitle.innerText = textData.techStackTitle;
+            englishTitle.innerText = textData.englishTitle;
+            english.innerText = textData.english;
+    
+            aboutMeArticle.innerHTML = "";
+            const aboutMeTitle = document.createElement("h1");
+            aboutMeTitle.innerText = textData.aboutMeTitle;
+            aboutMeArticle.appendChild(aboutMeTitle);
+    
+            const aboutMeTexts = textData.aboutMe;
+            for (const text of aboutMeTexts) {
+                const p = document.createElement("p");
+                p.innerHTML = text;
+                aboutMeTitle.insertAdjacentElement("afterend", p);
+            }
+        }
+
         let textData = {};
         const textDataPathHun = './text.hu.json';
         const textDataPathEng = './text.en.json';
@@ -27,26 +36,15 @@ async function _load() {
         renderTextData(textData);
     }
 
-    function renderTextData(textData) {
-        contactTitle.innerText = textData.contactTitle;
-        techStackTitle.innerText = textData.techStackTitle;
-        englishTitle.innerText = textData.englishTitle;
-        english.innerText = textData.english;
-
-        aboutMeArticle.innerHTML = "";
-        const aboutMeTitle = document.createElement("h1");
-        aboutMeTitle.innerText = textData.aboutMeTitle;
-        aboutMeArticle.appendChild(aboutMeTitle);
-
-        const aboutMeTexts = textData.aboutMe;
-        for (const text of aboutMeTexts) {
-            const p = document.createElement("p");
-            p.innerHTML = text;
-            aboutMeTitle.insertAdjacentElement("afterend", p);
-        }
-    }
+    const aboutMeArticle = document.querySelector('main > section:first-of-type > article:first-of-type');
+    const techStackTitle = document.querySelector('main > section:first-of-type > article:nth-of-type(2) > h1');
+    const englishTitle = document.querySelector('main > section:first-of-type > article:nth-of-type(3) > h1');
+    const english = document.querySelector('main > section:first-of-type > article:nth-of-type(3) > p');
+    const contactTitle = document.querySelector('main > aside > section > h3');
+    const langSwitch = document.querySelector('[type=checkbox]');
 
     langSwitch.addEventListener('change', langSwitchHandler);
+    langSwitchHandler();
 }
 
 window.addEventListener("load", _load);
